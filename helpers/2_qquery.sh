0619a3fb621ab2b99afcc4bfb0f0c5944c6b7f68
Pan Ziyue <random.rrr3@gmail.com>
2018-11-26 11:29:11 +0800

    updated helper scripts to fix bugs and improve comments

--- helpers/2_qquery.sh
+++ helpers/2_qquery.sh
@@ -9,6 +9,6 @@ export FACTOTUM_PVT_IP=$(grep ^FACTOTUM_PVT_IP ~/.sds/config | awk '{print $2}')
 export KEY_FILENAME=$(grep ^KEY_FILENAME ~/.sds/config | awk '{print $2}')
 
 export QQUERY_RELEASE_NUMBER=release-20180612 # This release number should be the same as the qquery SDS package
-export SLING_RELEASE_NUMBER=release-20180129 # This release number should be the same as the spyyyderman SDS package
+export SPYYYDERMAN_RELEASE_NUMBER=release-20180129 # This release number should be the same as the spyyyderman SDS package
 
-ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/qquery/qquery/cron.py --tag=$QQUERY_RELEASE_NUMBER --sling_tag=$SLING_RELEASE_NUMBER asf"
+ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/qquery/qquery/cron.py --tag=$QQUERY_RELEASE_NUMBER --sling_tag=$SPYYYDERMAN_RELEASE_NUMBER asf"
---
+++ helpers/3_scrape_s1_orbit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script scrapes and ingests Sentinel-1 orbital and calibration data from ESA
+# The origins of the commands can be traced back to https://github.com/hysds/ariamh/wiki/ARIA-Adaptation
+
+export FACTOTUM_PVT_IP=$(grep ^FACTOTUM_PVT_IP /home/ops/.sds/config | awk '{print $2}')
+export GRQ_PVT_IP=$(grep ^GRQ_PVT_IP /home/ops/.sds/config | awk '{print $2}')
+export KEY_FILENAME=$(grep ^KEY_FILENAME /home/ops/.sds/config | awk '{print $2}')
+
+export INGEST_RELEASE_NUMBER=release-20180619 # This number should correspond to hysds_s1_qc_ingest's release number
+
+ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/s1_qc_ingest/cron_crawler.py --type orbit --dataset_version v1.1 --tag $INGEST_RELEASE_NUMBER http://${GRQ_PVT_IP}:9200"
+ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/s1_qc_ingest/cron_crawler.py --type calibration --dataset_version v1.1 --tag $INGEST_RELEASE_NUMBER http://${GRQ_PVT_IP}:9200"
