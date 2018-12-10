986e59aced61ccd52f3e571bad3121a0d90a4a24
Pan Ziyue <random.rrr3@gmail.com>
2018-12-10 09:41:47 +0800

    updated hysds_s1_qc_ingest version number

--- helpers/3_scrape_s1_orbit.sh
+++ helpers/3_scrape_s1_orbit.sh
@@ -7,7 +7,7 @@ export FACTOTUM_PVT_IP=$(grep ^FACTOTUM_PVT_IP /home/ops/.sds/config | awk '{pri
 export GRQ_PVT_IP=$(grep ^GRQ_PVT_IP /home/ops/.sds/config | awk '{print $2}')
 export KEY_FILENAME=$(grep ^KEY_FILENAME /home/ops/.sds/config | awk '{print $2}')
 
-export INGEST_RELEASE_NUMBER=release-20180619 # This number should correspond to hysds_s1_qc_ingest's release number
+export INGEST_RELEASE_NUMBER=release-20180627 # This number should correspond to hysds_s1_qc_ingest's release number
 
 ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/s1_qc_ingest/cron_crawler.py --type orbit --dataset_version v1.1 --tag $INGEST_RELEASE_NUMBER http://${GRQ_PVT_IP}:9200"
 ssh -i $KEY_FILENAME ops@${FACTOTUM_PVT_IP} "source ~/verdi/bin/activate; ~/verdi/ops/s1_qc_ingest/cron_crawler.py --type calibration --dataset_version v1.1 --tag $INGEST_RELEASE_NUMBER http://${GRQ_PVT_IP}:9200"
